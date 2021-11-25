465b08ac2653398484e2bddfe35c0732fdb61f73
Felipe Amorim <felipeamorim.dev@gmail.com>
2021-11-25 19:15:40 -0300

    primeiro commit

---
+++ src/main/java/com/app/list_students/ListStudentsApplication.java
@@ -0,0 +1,13 @@
+package com.app.list_students;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListStudentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListStudentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/list_students/ListStudentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.list_students;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListStudentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
