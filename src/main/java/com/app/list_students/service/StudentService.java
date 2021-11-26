b22ae932e6309cda074d2ef976e97f9d89c21bc8
Felipe Amorim <felipeamorim.dev@gmail.com>
2021-11-26 17:04:27 -0300

    Finalizado a construção do CRUD para cadastro de estudantes

--- src/main/java/com/app/list_students/service/StudentService.java
+++ src/main/java/com/app/list_students/service/StudentService.java
@@ -7,4 +7,12 @@ import com.app.list_students.entities.Student;
 public interface StudentService {
 	
 	List<Student> getAllStudents();
+	
+	Student saveStudent(Student student);
+	
+	Student getStudentById(Long id);
+	
+	Student updateStudent(Student student);
+	
+	void deleteStudentById(Long id);
 }
--- src/main/java/com/app/list_students/service/StudentServiceImpl.java
+++ src/main/java/com/app/list_students/service/StudentServiceImpl.java
@@ -9,7 +9,7 @@ import com.app.list_students.repository.StudentRepository;
 
 @Service
 public class StudentServiceImpl implements StudentService {
-	
+
 	private StudentRepository studentRepository;
 
 	public StudentServiceImpl(StudentRepository studentRepository) {
@@ -19,9 +19,28 @@ public class StudentServiceImpl implements StudentService {
 
 	@Override
 	public List<Student> getAllStudents() {
-		return studentRepository.findAll(); 
+		return studentRepository.findAll();
 	}
-	
-	
-	
+
+	@Override
+	public Student saveStudent(Student student) {
+		return studentRepository.save(student);
+	}
+
+	@Override
+	public Student getStudentById(Long id) {
+		return studentRepository.findById(id).get();
+	}
+
+	@Override
+	public Student updateStudent(Student student) {
+		return studentRepository.save(student);
+	}
+
+	@Override
+	public void deleteStudentById(Long id) {
+		studentRepository.deleteById(id);
+
+	}
+
 }
